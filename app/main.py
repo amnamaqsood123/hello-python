1ee46013a0b14b5301b5c34670b0ea846653b608
amnamaqsood123 <150299412+amnamaqsood123@users.noreply.github.com>
2024-03-26 10:46:35 +0000

    main.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,23 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request
+import datetime
 
-@app.route("/")
-def hello():
-    return "Hello from Python!"
+app = Flask(__student_data_app__)
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+@app.route('/')
+def index():
+    # Get current time and date
+    now = datetime.datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    current_date = now.strftime("%Y-%m-%d")
+    
+    
+    student_name = "Amna Maqsood"
+    
+    
+    student_id = "S2264480"
+    
+    return f"Hello, {student_name}!<br>Your Student ID is: {student_id}.<br>Current time: {current_time}<br>Current date: {current_date}"
+
+if __student_data_app__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
+
